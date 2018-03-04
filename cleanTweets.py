d9f5489e16609344ea7acd2d91a72d9a032ba74a
greysonevins <greyman1995@gmail.com>
2018-03-04 16:53:49 +0000

    updates to clean to normalize by state population

--- cleanTweets.py
+++ cleanTweets.py
@@ -19,6 +19,17 @@ STATES_CODES = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
           "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
           "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
+# "Annual Estimates of the Resident Population for the United States, Regions, States, and Puerto Rico: April 1, 2010 to July 1, 2016" (XLSX). United States Census Bureau. Retrieved 8 June 2017.
+STATES_POPULATION = {"Alabama": 4863300,"Alaska": 741894,"Arizona": 6931071,"Arkansas": 2988248,"California": 39250017,"Colorado": 5540545,
+  "Connecticut": 3576452,"Delaware": 952065,"Florida": 20612439,"Georgia": 10310371,"Hawaii": 1428557,"Idaho": 1683140,"Illinois": 12801539,
+  "Indiana": 6633053,"Iowa": 3134693,"Kansas": 2907289,"Kentucky": 4436974,"Louisiana": 4681666,"Maine": 1331479,"Maryland": 6016447,
+  "Massachusetts": 6811779,"Michigan": 9928300,"Minnesota": 5519952,"Mississippi": 2988726,"Missouri": 6093000,"Montana": 1042520,
+  "Nebraska": 1907116,"Nevada": 2940058,"New Hampshire": 1334795,"New Jersey": 8944469,"New Mexico": 2081015,"New York": 19745289,
+  "North Carolina": 10146788,"North Dakota": 757952,"Ohio": 11614373,"Oklahoma": 3923561,"Oregon": 4093465,"Pennsylvania": 12784227,
+  "Rhode Island": 1056426,"South Carolina": 4961119,"South Dakota": 865454,"Tennessee": 6651194,"Texas": 27862596,"Utah": 3051217,
+  "Vermont": 624594,"Virginia": 8411808,"Washington": 7288000,"West Virginia": 1831102,"Wisconsin": 5778708,"Wyoming": 585501}
+
+
 def cleanTweets():
     tweets = []
 
@@ -106,7 +117,16 @@ def statesAmounts():
       "Rhode Island": 0,"South Carolina": 0,"South Dakota": 0,"Tennessee": 0,"Texas": 0,"Utah": 0,
       "Vermont": 0,"Virginia": 0,"Washington": 0,"West Virginia": 0,"Wisconsin": 0,"Wyoming":0}
 
-    days = ['2018-02-28','2018-03-01','2018-03-02']
+    State_Tweets_day4 = {"Alabama": 0,"Alaska": 0,"Arizona": 0,"Arkansas": 0,"California": 0,"Colorado": 0,
+      "Connecticut": 0,"Delaware": 0,"Florida": 0,"Georgia": 0,"Hawaii": 0,"Idaho": 0,"Illinois": 0,
+      "Indiana": 0,"Iowa": 0,"Kansas": 0,"Kentucky": 0,"Louisiana": 0,"Maine": 0,"Maryland": 0,
+      "Massachusetts": 0,"Michigan": 0,"Minnesota": 0,"Mississippi": 0,"Missouri": 0,"Montana": 0,
+      "Nebraska": 0,"Nevada": 0,"New Hampshire": 0,"New Jersey": 0,"New Mexico": 0,"New York": 0,
+      "North Carolina": 0,"North Dakota": 0,"Ohio": 0,"Oklahoma": 0,"Oregon": 0,"Pennsylvania": 0,
+      "Rhode Island": 0,"South Carolina": 0,"South Dakota": 0,"Tennessee": 0,"Texas": 0,"Utah": 0,
+      "Vermont": 0,"Virginia": 0,"Washington": 0,"West Virginia": 0,"Wisconsin": 0,"Wyoming":0}
+
+    days = ['2018-02-27', '2018-02-28','2018-03-01','2018-03-02']
     dayCount = 1
 
     for tweet in tweetsUsed:
@@ -128,6 +148,17 @@ def statesAmounts():
                 valuePast = State_Tweets_day3[locationUsed]
                 valuePast += 1
                 State_Tweets_day3[locationUsed]  = valuePast
+            elif dayTweeted == 3:
+                valuePast = State_Tweets_day4[locationUsed]
+                valuePast += 1
+                State_Tweets_day4[locationUsed]  = valuePast
+
+
+    State_Tweets_day1 = {key: State_Tweets_day1[key] / population for key, population in STATES_POPULATION.items()}
+    State_Tweets_day2 = {key: State_Tweets_day2[key] / population for key, population in STATES_POPULATION.items()}
+    State_Tweets_day3 = {key: State_Tweets_day3[key] / population for key, population in STATES_POPULATION.items()}
+    State_Tweets_day4 = {key: State_Tweets_day4[key] / population for key, population in STATES_POPULATION.items()}
+
     with open("day1Tweets.json", "w") as total:
         json.dump(State_Tweets_day1, total)
 
@@ -136,6 +167,9 @@ def statesAmounts():
 
     with open("day3Tweets.json", "w") as total:
         json.dump(State_Tweets_day3, total)
+
+    with open("day4Tweets.json", "w") as total:
+        json.dump(State_Tweets_day4, total)
 
 
 cleanTweets()
